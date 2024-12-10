49bd1985e2e064a97d5bc9cd5fa43476262b08cb
pablomn02 <pablomnavarro9@gmail.com>
2024-12-11 00:49:38 +0100

    Barra de navegacion y primeras paginas implementadas

--- KeymotionApp/src/app/app.module.ts
+++ KeymotionApp/src/app/app.module.ts
@@ -2,13 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { RouterLink, RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { VehiculosComponent } from './pages/vehiculos/vehiculos.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { FavoritosComponent } from './pages/favoritos/favoritos.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'vehiculos', component: VehiculosComponent },
+  { path: 'favoritos', component: FavoritosComponent },
+  { path: 'contacto', component: ContactoComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    VehiculosComponent,
+    ContactoComponent,
+    FavoritosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
